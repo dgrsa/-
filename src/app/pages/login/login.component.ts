982e33e4d38ed2a3e302338e9f2d1d963fbfad4e
hanygamal72 <hany.gamal290@gmail.com>
2021-03-25 12:15:27 +0200

    pages/login: design resetPassword modal

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  imports: [CommonModule, LoginRoutingModule, ModalModule.forRoot()],
 })
-export class LoginModule { }
+export class LoginModule {}
