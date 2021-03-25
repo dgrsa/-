9cfca59ed8d23c4ccafe8ceab97c19f0bd234f73
hanygamal72 <hany.gamal290@gmail.com>
2021-03-25 09:28:48 +0200

    shared/services/language: add languageEmmitter

---
+++ src/app/shared/services/language-emmiter.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LanguageEmitterService {
+  private emitChangeSource = new Subject<any>();
+  changeEmitted$ = this.emitChangeSource.asObservable();
+  constructor() {}
+
+  emitChange(change: any) {
+    this.emitChangeSource.next(change);
+  }
+}
