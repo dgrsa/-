0699ab75dfccab2d820d8e940b74d9e944cff7b7
hanygamal72 <hany.gamal290@gmail.com>
2021-03-29 11:17:22 +0200

    shared/components/table-modal: design component

--- src/app/shared/components/table-modal/table-modal.component.ts
+++ src/app/shared/components/table-modal/table-modal.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-table-modal',
   templateUrl: './table-modal.component.html',
-  styleUrls: ['./table-modal.component.scss']
+  styleUrls: ['./table-modal.component.scss'],
 })
 export class TableModalComponent implements OnInit {
+  constructor(
+    public bsModalRef: BsModalRef,
+    public translate: TranslateService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
