5becf508ab0ba6d9a610fda138e1f2dacfb1e5b3
hanygamal72 <hany.gamal290@gmail.com>
2021-03-29 10:04:47 +0200

    shared/components/cart: design compoent

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  constructor(public bsModalRef: BsModalRef) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
