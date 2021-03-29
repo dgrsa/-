81f704e2182bc14e0bf0952d2e42ea23d0d16dd1
hanygamal72 <hany.gamal290@gmail.com>
2021-03-29 13:27:56 +0200

    pages/order/tracking: intialize component and route

--- src/app/pages/order/order-routing.module.ts
+++ src/app/pages/order/order-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tracking', component: OrderTrackingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class OrderRoutingModule { }
+export class OrderRoutingModule {}
--- src/app/pages/order/order.module.ts
+++ src/app/pages/order/order.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
+import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [OrderTrackingComponent],
   imports: [
     CommonModule,
     OrderRoutingModule
