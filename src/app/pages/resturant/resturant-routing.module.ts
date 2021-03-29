7f6f933d3887d69c628eb04b801171ec1031fa8b
hanygamal72 <hany.gamal290@gmail.com>
2021-03-29 12:00:59 +0200

    pages/resturant/meal: intialize component

--- src/app/pages/resturant/resturant-routing.module.ts
+++ src/app/pages/resturant/resturant-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsComponent } from './details/details.component';
+import { MealDetailsComponent } from './meal-details/meal-details.component';
 
-const routes: Routes = [{ path: 'details', component: DetailsComponent }];
+const routes: Routes = [
+  { path: 'details', component: DetailsComponent },
+  { path: 'meal-details', component: MealDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/resturant/resturant.module.ts
+++ src/app/pages/resturant/resturant.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ResturantRoutingModule } from './resturant-routing.module';
 import { DetailsComponent } from './details/details.component';
 import { SwiperModule } from 'ngx-swiper-wrapper';
+import { MealDetailsComponent } from './meal-details/meal-details.component';
 
 @NgModule({
-  declarations: [DetailsComponent],
+  declarations: [DetailsComponent, MealDetailsComponent],
   imports: [CommonModule, ResturantRoutingModule, SwiperModule],
 })
 export class ResturantModule {}
