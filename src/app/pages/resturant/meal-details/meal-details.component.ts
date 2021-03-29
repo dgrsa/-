6ed2635c21b974a2254e8bd2d4d14589dc0f07f5
hanygamal72 <hany.gamal290@gmail.com>
2021-03-29 12:32:19 +0200

    pages/resturant/meal-details: design component

--- src/app/pages/resturant/meal-details/meal-details.component.ts
+++ src/app/pages/resturant/meal-details/meal-details.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-meal-details',
   templateUrl: './meal-details.component.html',
-  styleUrls: ['./meal-details.component.scss']
+  styleUrls: ['./meal-details.component.scss'],
 })
 export class MealDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    navigation: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    // autoplay: true,
+    keyboard: true,
+    scrollbar: false,
+    loop: true,
+    speed: 700,
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+  };
 }
