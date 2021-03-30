ca915e967f3f95a0dee298fee4a3e2d4cb8b3284
hanygamal72 <hany.gamal290@gmail.com>
2021-03-30 12:57:59 +0200

    pages/order/orders: design component

--- src/app/pages/order/order.module.ts
+++ src/app/pages/order/order.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
 import { OrdersComponent } from './orders/orders.component';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [OrderTrackingComponent, OrdersComponent],
-  imports: [
-    CommonModule,
-    OrderRoutingModule
-  ]
+  imports: [CommonModule, OrderRoutingModule, TabsModule.forRoot()],
 })
-export class OrderModule { }
+export class OrderModule {}
