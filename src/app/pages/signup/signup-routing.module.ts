44ba13e73aef00b857e74869d5920479cb7507b4
hanygamal72 <hany.gamal290@gmail.com>
2021-04-04 14:32:27 +0200

    pages/signup/create-password: intialize component

--- src/app/pages/signup/signup-routing.module.ts
+++ src/app/pages/signup/signup-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePasswordComponent } from './create-password/create-password.component';
 import { SignupComponent } from './signup.component';
 
-const routes: Routes = [{ path: '', component: SignupComponent }];
+const routes: Routes = [
+  { path: '', component: SignupComponent },
+  { path: 'create-password', component: CreatePasswordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CreatePasswordComponent } from './create-password/create-password.component';
 
 @NgModule({
-  declarations: [SignupComponent],
+  declarations: [SignupComponent, CreatePasswordComponent],
   imports: [CommonModule, SignupRoutingModule, TranslateModule.forChild()],
 })
 export class SignupModule {}
