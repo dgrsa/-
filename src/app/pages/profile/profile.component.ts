040622cb8bdd8c863ee4999cb91aeaa9108ec2e4
hanygamal72 <hany.gamal290@gmail.com>
2021-04-05 14:13:17 +0200

    pages/profile: get and display user data

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public mainInfoForm = new FormGroup({
+    phone: new FormControl({ value: '', disabled: true }),
+    name: new FormControl(),
+    email: new FormControl([Validators.email]),
+  });
+  userData: {};
+  constructor(
+    private spinner: NgxSpinnerService,
+    private authService: AuthService,
+    private cookieService: CookieService
+  ) {
+    this.userData = environment.userInfo;
+    this.mainInfoForm.patchValue(this.userData);
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule, TranslateModule.forChild()],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    TranslateModule.forChild(),
+    ReactiveFormsModule,
+  ],
 })
 export class ProfileModule {}
