8379ce3064023593a8bc804b990232236bf2c33a
hanygamal72 <hany.gamal290@gmail.com>
2021-04-05 12:50:07 +0200

    pages/login: implement forogt password func

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,6 +7,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AuthService } from 'src/app/shared/auth/auth.service';
 import { HelperToolsService } from 'src/app/shared/services/helper-tools.service';
+import { ValidateFormService } from 'src/app/shared/services/validate-form.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -21,6 +22,9 @@ export class LoginComponent implements OnInit {
     password: new FormControl('', Validators.required),
     rememberMe: new FormControl(''),
   });
+  public passwordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
   expiredIn: Date;
   constructor(
     private modalService: BsModalService,
@@ -29,7 +33,8 @@ export class LoginComponent implements OnInit {
     private cookieService: CookieService,
     private authService: AuthService,
     private helperTool: HelperToolsService,
-    private router: Router
+    private router: Router,
+    private validForm: ValidateFormService
   ) {}
 
   openModal(template: TemplateRef<any>) {
@@ -39,54 +44,105 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   loginUser(): void {
-    this.spinner.show();
-    const sentDat = {};
-    sentDat['phone'] = this.loginFrom.value.phone;
-    sentDat['password'] = this.loginFrom.value.password;
-    this.authService.loginUser(sentDat).subscribe(
-      (data) => {
-        if (data['success']) {
-          this.expiredIn = new Date(+data['data']['token']['expiresIn'] * 1000);
-          environment.userInfo = data['data']['user'];
-          this.authService.emitChange({
-            name: 'user_login',
-            user_data: data['data']['user'],
-          });
-          this.authService.emitTokenChange(
-            data['data']['token']['accessToken']
-          );
-          this.cookieService.set(
-            'Btoken',
-            data['data']['token']['accessToken'],
-            this.expiredIn
-          );
-          this.cookieService.set(
-            'BuserId',
-            data['data']['user']['id'],
-            this.expiredIn
-          );
-          this.router.navigate(['/']);
+    if (this.loginFrom.valid) {
+      this.spinner.show();
+      const sentDat = {};
+      sentDat['phone'] = this.loginFrom.value.phone;
+      sentDat['password'] = this.loginFrom.value.password;
+      this.authService.loginUser(sentDat).subscribe(
+        (data) => {
+          if (data['success']) {
+            this.expiredIn = new Date(
+              +data['data']['token']['expiresIn'] * 1000
+            );
+            environment.userInfo = data['data']['user'];
+            this.authService.emitChange({
+              name: 'user_login',
+              user_data: data['data']['user'],
+            });
+            this.authService.emitTokenChange(
+              data['data']['token']['accessToken']
+            );
+            this.cookieService.set(
+              'Btoken',
+              data['data']['token']['accessToken'],
+              this.expiredIn
+            );
+            this.cookieService.set(
+              'BuserId',
+              data['data']['user']['id'],
+              this.expiredIn
+            );
+            this.router.navigate(['/']);
+            this.spinner.hide();
+          }
+        },
+        (err) => {
+          console.log(err);
+          if (err['error']['message'] == 'controllers.auth.notActiveAccount') {
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'This account has not been activated yet',
+              'error'
+            );
+          }
+          if (
+            err['error']['message'] == 'controllers.auth.incorrectCredential'
+          ) {
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'Account not found or incorrect credential.',
+              'error'
+            );
+          }
           this.spinner.hide();
         }
-      },
-      (err) => {
-        console.log(err);
-        if (err['error']['message'] == 'controllers.auth.notActiveAccount') {
-          this.helperTool.showAlertWithTranslation(
-            '',
-            'This account has not been activated yet',
-            'error'
-          );
+      );
+    } else {
+      this.validForm.validateAllFormFields(this.loginFrom);
+    }
+  }
+
+  onEmailSubmit() {
+    if (this.passwordForm.valid) {
+      this.spinner.show();
+      this.authService.sendCode(this.passwordForm.value).subscribe(
+        (data) => {
+          if (data['success']) {
+            this.spinner.hide();
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'A message was sent to your email',
+              'success'
+            );
+            this.router.navigate(['/password/reset']);
+            this.cookieService.set('userEmail', this.passwordForm.value.email);
+          }
+        },
+        (err) => {
+          this.spinner.hide();
+          if (err['error']['message'] == 'controller.emailNotFound') {
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'This email was not found',
+              'error'
+            );
+          }
+          if (
+            err['error']['errors']['length'] &&
+            err['error']['errors'][0]['message'] ==
+              `"email" must be a valid email`
+          ) {
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'Email must be a valid email',
+              'error'
+            );
+          }
         }
-        if (err['error']['message'] == 'controllers.auth.incorrectCredential') {
-          this.helperTool.showAlertWithTranslation(
-            '',
-            'Account not found or incorrect credential.',
-            'error'
-          );
-        }
-        this.spinner.hide();
-      }
-    );
+      );
+    } else {
+      this.validForm.validateAllFormFields(this.passwordForm);
+    }
   }
 }
