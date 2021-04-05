8aa4abe6238dc84046c24bd29c16728ae0ae9b87
hanygamal72 <hany.gamal290@gmail.com>
2021-04-05 13:47:18 +0200

    pages/password/reset: implement resetPassword func

--- src/app/pages/password/password.module.ts
+++ src/app/pages/password/password.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PasswordRoutingModule } from './password-routing.module';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ResetPasswordComponent],
-  imports: [CommonModule, PasswordRoutingModule, TranslateModule.forChild()],
+  imports: [
+    CommonModule,
+    PasswordRoutingModule,
+    TranslateModule.forChild(),
+    ReactiveFormsModule,
+  ],
 })
 export class PasswordModule {}
--- src/app/pages/password/reset-password/reset-password.component.ts
+++ src/app/pages/password/reset-password/reset-password.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { HelperToolsService } from 'src/app/shared/services/helper-tools.service';
+import { ValidateFormService } from 'src/app/shared/services/validate-form.service';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
+  public passwordForm = new FormGroup({
+    passwordResetToken: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  constructor(
+    private spinner: NgxSpinnerService,
+    private cookieService: CookieService,
+    private authService: AuthService,
+    private helperTool: HelperToolsService,
+    private router: Router,
+    private validForm: ValidateFormService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  resetPassword() {
+    if (this.passwordForm.valid) {
+      this.spinner.show();
+      const sentData = this.passwordForm.value;
+      sentData['email'] = 'hany.gamal290@gmail.com';
+      this.authService.resetPassword(sentData).subscribe(
+        (data) => {
+          if (data['success']) {
+            // console.log(data);
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'Password reset',
+              'success'
+            );
+            this.spinner.hide();
+            this.router.navigate(['/']);
+          }
+        },
+        (err) => {
+          this.spinner.hide();
+          console.error(err);
+          if (
+            err['error']['message'] == 'controller.changePassword.invalidToken'
+          ) {
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'Code is incorrect',
+              'error'
+            );
+          }
+        }
+      );
+    } else {
+      this.validForm.validateAllFormFields(this.passwordForm);
+    }
   }
-
 }
