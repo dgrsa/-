6bec54cb2472871888ecdc1be561537412cdae98
hanygamal72 <hany.gamal290@gmail.com>
2021-04-05 14:46:56 +0200

    pages/profile: implement updateUserData function

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -4,6 +4,8 @@ import { CookieService } from 'ngx-cookie-service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/shared/auth/auth.service';
+import { HelperToolsService } from 'src/app/shared/services/helper-tools.service';
+import { ValidateFormService } from 'src/app/shared/services/validate-form.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -21,11 +23,52 @@ export class ProfileComponent implements OnInit {
   constructor(
     private spinner: NgxSpinnerService,
     private authService: AuthService,
-    private cookieService: CookieService
+    private cookieService: CookieService,
+    private helperTool: HelperToolsService,
+    private validateService: ValidateFormService
   ) {
     this.userData = environment.userInfo;
     this.mainInfoForm.patchValue(this.userData);
   }
 
   ngOnInit(): void {}
+
+  updateUserData() {
+    if (this.mainInfoForm.valid) {
+      this.spinner.show();
+      this.authService
+        .updateUserdata(this.userData['id'], {
+          email: this.mainInfoForm.value.email,
+          name: this.mainInfoForm.value.name,
+        })
+        .subscribe(
+          (data) => {
+            if (data['success']) {
+              this.helperTool.showAlertWithTranslation(
+                '',
+                'User data has been updated',
+                'success'
+              );
+              this.spinner.hide();
+            } else {
+            }
+          },
+          (err) => {
+            this.spinner.hide();
+            if (
+              err['error']['errors'][0]['message'] == 'email is already existed'
+            ) {
+              this.helperTool.showAlertWithTranslation(
+                '',
+                'Email is already existed',
+                'error'
+              );
+            }
+            console.log(err);
+          }
+        );
+    } else {
+      this.validateService.validateAllFormFields(this.mainInfoForm);
+    }
+  }
 }
