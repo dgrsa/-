baa9c5e1cd1c4530afa75f78d45e0ece2730b780
hanygamal72 <hany.gamal290@gmail.com>
2021-04-06 14:32:38 +0200

    use firebaseConfig of seelaz and will updat it later

---
+++ src/app/fireConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAp2G1jIw0iKrmiqa5X4mebgscnLGasARs",
+  authDomain: "seelaz-1ca97.firebaseapp.com",
+  databaseURL: "https://seelaz-1ca97.firebaseio.com",
+  projectId: "seelaz-1ca97",
+  storageBucket: "seelaz-1ca97.appspot.com",
+  messagingSenderId: "993225166032",
+  appId: "1:993225166032:web:0dcb2378d2aac09d7e22d5"
+};
