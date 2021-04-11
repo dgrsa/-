63ea675506bd39075e128824e29824e734e7672a
hanygamal72 <hany.gamal290@gmail.com>
2021-04-11 15:04:09 +0200

    component/cart: display cartItems

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { environment } from 'src/environments/environment';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +10,33 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  constructor(public bsModalRef: BsModalRef) {}
+  meal_data = [];
+  cart_data = {} as any;
+  imageBaseURL = environment.imageBaseUrl;
+  constructor(
+    public bsModalRef: BsModalRef,
+    private cartService: CartService,
+    public translate: TranslateService
+  ) {
+    this.meal_data = this.cartService.CartData['mealsData'];
+    this.cart_data = this.cartService.CartData;
+  }
 
   ngOnInit(): void {}
+
+  deleteCatrItem(index) {
+    this.cartService.onDeleteCliced(index);
+  }
+
+  onChangeQuantity(Data, value, i) {
+    if (value == -1) {
+      Data.quantity += value;
+      Data.price = Data.quantity * this.meal_data[i].price;
+      this.cartService.UpdateCart();
+    } else if (value == 1) {
+      Data.quantity += value;
+      Data.price = Data.quantity * this.meal_data[i].price;
+      this.cartService.UpdateCart();
+    }
+  }
 }
