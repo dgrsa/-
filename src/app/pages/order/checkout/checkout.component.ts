a09d086bbf1915ba8ee943097bd71c2142e7a2c9
hanygamal72 <hany.gamal290@gmail.com>
2021-04-19 16:07:52 +0200

    pages/order/checkout: get and display payment

--- src/app/pages/order/checkout/checkout.component.ts
+++ src/app/pages/order/checkout/checkout.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { GeneralService } from 'src/app/shared/services/general.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  methods = [];
+  imageBaseUrl = environment.imageBaseUrl;
+  constructor(
+    private spinner: NgxSpinnerService,
+    private generalService: GeneralService,
+    private coockieService: CookieService
+  ) {}
 
   ngOnInit(): void {
+    this.getPaymentMethods();
   }
 
+  getPaymentMethods(): void {
+    this.spinner.show();
+    this.generalService.getPayments(undefined).subscribe(
+      (data) => {
+        if (data['success']) {
+          this.methods = data['data']['rows'];
+          this.spinner.hide();
+        }
+      },
+      (err) => {
+        console.error(err);
+        this.spinner.hide();
+      }
+    );
+  }
 }
