3e2efc29a900548828911b5e728710b63c2b8c11
hanygamal72 <hany.gamal290@gmail.com>
2021-04-27 12:06:30 +0200

    update firbase configration

--- src/app/fireConfig.ts
+++ src/app/fireConfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAp2G1jIw0iKrmiqa5X4mebgscnLGasARs",
-  authDomain: "seelaz-1ca97.firebaseapp.com",
-  databaseURL: "https://seelaz-1ca97.firebaseio.com",
-  projectId: "seelaz-1ca97",
-  storageBucket: "seelaz-1ca97.appspot.com",
-  messagingSenderId: "993225166032",
-  appId: "1:993225166032:web:0dcb2378d2aac09d7e22d5"
+  apiKey: 'AIzaSyCtnfnYSxLHQPZb8SYVfq4723FbA5acnQ0',
+  authDomain: 'brodone-a9a3a.firebaseapp.com',
+  projectId: 'brodone-a9a3a',
+  storageBucket: 'brodone-a9a3a.appspot.com',
+  messagingSenderId: '822531144101',
+  appId: '1:822531144101:web:ce36c2bb2f8a91144f104c',
+  measurementId: 'G-BR22J3PW33',
 };
