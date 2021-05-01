c334712838e7b607b0c5f989d3712cc8fdc7212e
hanygamal72 <hany.gamal290@gmail.com>
2021-05-02 01:53:20 +0200

    components/scan-code: get the right table number

--- src/app/shared/components/scan-code/scan-code.component.ts
+++ src/app/shared/components/scan-code/scan-code.component.ts
@@ -61,6 +61,10 @@ export class ScanCodeComponent implements OnInit {
     this.qrScannerComponent.capturedQr.subscribe((result) => {
       if (this.previousUrl) {
         this.cartService.tableNumber = result.split('?table=', 2);
+        this.cartService.tableNumber = this.cartService.tableNumber[1].slice(
+          0,
+          1
+        );
         this.router.navigate([this.previousUrl]);
       } else {
         if (result.includes('https://')) {
