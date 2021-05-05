e8810d327911ac35020b0e3369975b91fdf86562
hanygamal72 <hany.gamal290@gmail.com>
2021-05-05 14:55:33 +0200

    pages/resturant: get and display special and all res

--- src/app/pages/resturant/resturant.component.ts
+++ src/app/pages/resturant/resturant.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { GeneralService } from 'src/app/shared/services/general.service';
+import { ResturantService } from 'src/app/shared/services/resturant.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-resturant',
   templateUrl: './resturant.component.html',
-  styleUrls: ['./resturant.component.scss']
+  styleUrls: ['./resturant.component.scss'],
 })
 export class ResturantComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  offset = 1;
+  resturants = [];
+  Specialresturants = [];
+  counter;
+  page;
+  imageBaseURL = environment.imageBaseUrl;
+  constructor(
+    private spinner: NgxSpinnerService,
+    private resturantService: ResturantService,
+    private route: ActivatedRoute
+  ) {
+    route.queryParams.subscribe((params) => {
+      if (params['special']) {
+        this.getSpecialResturant();
+      } else {
+        this.getResturants;
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  getSpecialResturant(): void {
+    this.spinner.show();
+    this.resturantService.getResturant(this.offset - 1, true).subscribe(
+      (data) => {
+        if (data['success']) {
+          this.spinner.hide();
+          this.counter = data['data']['count'];
+          this.Specialresturants = data['data']['rows'];
+        }
+      },
+      (err) => {
+        this.spinner.hide();
+        console.error(err);
+      }
+    );
+  }
+
+  getResturants(): void {
+    this.spinner.show();
+    this.resturantService.getResturant(this.offset - 1, undefined).subscribe(
+      (data) => {
+        if (data['success']) {
+          this.spinner.hide();
+          this.counter = data['data']['count'];
+          this.resturants = data['data']['rows'];
+        }
+      },
+      (err) => {
+        this.spinner.hide();
+        console.error(err);
+      }
+    );
+  }
+
+  loadMore(event, type): void {
+    this.offset = event;
+    this.page = event;
+    if (type == 'special') {
+      this.getSpecialResturant();
+    } else {
+      this.getResturants();
+    }
+  }
 }
--- src/app/pages/resturant/resturant.module.ts
+++ src/app/pages/resturant/resturant.module.ts
@@ -8,6 +8,7 @@ import { MealDetailsComponent } from './meal-details/meal-details.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ResturantComponent } from './resturant.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [DetailsComponent, MealDetailsComponent, ResturantComponent],
@@ -17,6 +18,7 @@ import { ResturantComponent } from './resturant.component';
     SwiperModule,
     TranslateModule.forChild(),
     ReactiveFormsModule,
+    NgxPaginationModule,
   ],
 })
 export class ResturantModule {}
