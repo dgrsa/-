940e8507f95f763418a80db6a7703f6504c37ab2
hanygamal72 <hany.gamal290@gmail.com>
2021-05-11 22:46:23 +0200

    implement callWaiter func in order and resturant

--- src/app/pages/order/order-tracking/order-tracking.component.ts
+++ src/app/pages/order/order-tracking/order-tracking.component.ts
@@ -5,6 +5,8 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { CookieService } from 'ngx-cookie-service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AuthService } from 'src/app/shared/auth/auth.service';
+import { GeneralService } from 'src/app/shared/services/general.service';
+import { HelperToolsService } from 'src/app/shared/services/helper-tools.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -17,13 +19,16 @@ export class OrderTrackingComponent implements OnInit {
   orderData = {} as any;
   items = [];
   ImageBaseUrl = environment.imageBaseUrl;
+
   constructor(
     private modalService: BsModalService,
     public translate: TranslateService,
     private spinner: NgxSpinnerService,
     private cookieService: CookieService,
     private authService: AuthService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private generalService: GeneralService,
+    private helperTool: HelperToolsService
   ) {
     this.route.params.subscribe((params) => {
       this.getOrders(params['id']);
@@ -51,6 +56,32 @@ export class OrderTrackingComponent implements OnInit {
           if (data['success']) {
             this.orderData = data['data']['rows'][0];
             this.items = this.orderData.items;
+          }
+        },
+        (err) => {
+          this.spinner.hide();
+          console.error(err);
+        }
+      );
+  }
+
+  callWaiter(): void {
+    this.spinner.show();
+    this.generalService
+      .callWaiter(
+        this.orderData.resturant_id,
+        this.cookieService.get('tableId')
+      )
+      .subscribe(
+        (data) => {
+          this.spinner.hide();
+          if (data['success']) {
+            console.log(data);
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'A notification has been sent to a waiter',
+              'info'
+            );
           }
         },
         (err) => {
--- src/app/shared/components/table-modal/table-modal.component.ts
+++ src/app/shared/components/table-modal/table-modal.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { environment } from 'src/environments/environment';
+import { GeneralService } from '../../services/general.service';
+import { HelperToolsService } from '../../services/helper-tools.service';
 
 @Component({
   selector: 'app-table-modal',
@@ -13,8 +16,34 @@ export class TableModalComponent implements OnInit {
   imageBaseURL = environment.imageBaseUrl;
   constructor(
     public bsModalRef: BsModalRef,
-    public translate: TranslateService
+    public translate: TranslateService,
+    private spinner: NgxSpinnerService,
+    private generalService: GeneralService,
+    private helperTool: HelperToolsService
   ) {}
 
   ngOnInit(): void {}
+
+  callWaiter(): void {
+    this.spinner.show();
+    this.generalService
+      .callWaiter(this.table.resturant_id, this.table.id)
+      .subscribe(
+        (data) => {
+          this.spinner.hide();
+          if (data['success']) {
+            console.log(data);
+            this.helperTool.showAlertWithTranslation(
+              '',
+              'A notification has been sent to a waiter',
+              'info'
+            );
+          }
+        },
+        (err) => {
+          this.spinner.hide();
+          console.error(err);
+        }
+      );
+  }
 }
