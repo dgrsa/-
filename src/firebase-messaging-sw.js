ef671c474f598a30cb3c446b625123782d695c27
hanygamal72 <hany.gamal290@gmail.com>
2021-05-17 11:43:52 +0200

    add firbase-messaging and import it

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js"
+);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCtnfnYSxLHQPZb8SYVfq4723FbA5acnQ0",
+  authDomain: "brodone-a9a3a.firebaseapp.com",
+  projectId: "brodone-a9a3a",
+  storageBucket: "brodone-a9a3a.appspot.com",
+  messagingSenderId: "822531144101",
+  appId: "1:822531144101:web:ce36c2bb2f8a91144f104c",
+  measurementId: "G-BR22J3PW33",
+});
+
+const messaging = firebase.messaging();
+// messaging.onMessage((payload) => {
+//   console.log('Message received. ', payload);
+//   // ...
+// });
