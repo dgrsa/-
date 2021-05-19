6d1747627448b082e1133f1c17beef976060e36e
hanygamal72 <hany.gamal290@gmail.com>
2021-05-19 15:55:35 +0200

    pages/notifications: get and display notifications

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from 'src/app/shared/auth/auth.service';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
-
-  constructor() { }
+  notifications = [];
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService
+  ) {}
 
   ngOnInit(): void {
+    this.getNotifications();
   }
 
+  getNotifications(): void {
+    this.authService
+      .getNotifications(0, 100, this.cookieService.get('BuserId'))
+      .subscribe(
+        (data) => {
+          if (data['success']) {
+            this.notifications = data['data'];
+          }
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+  }
 }
