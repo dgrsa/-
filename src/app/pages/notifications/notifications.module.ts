3db599791f8630b16e6cdc9bd2dff5739500eb4a
hanygamal72 <hany.gamal290@gmail.com>
2021-05-19 13:08:16 +0200

    pages/notifications: design component

--- src/app/pages/notifications/notifications.module.ts
+++ src/app/pages/notifications/notifications.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { NotificationsRoutingModule } from './notifications-routing.module';
 import { NotificationsComponent } from './notifications.component';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [NotificationsComponent],
   imports: [
     CommonModule,
-    NotificationsRoutingModule
-  ]
+    NotificationsRoutingModule,
+    TranslateModule.forChild(),
+  ],
 })
-export class NotificationsModule { }
+export class NotificationsModule {}
