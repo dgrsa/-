786c6e04e849fe5ddccd78161bf3f8671a0582eb
hanygamal72 <hany.gamal290@gmail.com>
2021-05-30 14:27:34 +0200

    shared/components/footer: get and display socialLinks

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from 'src/environments/environment';
+import { GeneralService } from '../../services/general.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  settings = {} as any;
+  constructor(
+    private generalService: GeneralService,
+    public transalte: TranslateService
+  ) {}
 
   ngOnInit(): void {
+    this.getSittings();
   }
 
+  getSittings(): void {
+    this.generalService.getSettings().subscribe(
+      (data) => {
+        if (data['success']) {
+          this.settings = data['data'];
+          this.generalService.emitChange(data['data']);
+        }
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
 }
