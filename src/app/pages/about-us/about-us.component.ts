4632046b8c227edde51611f0325def4b05d0bd01
hanygamal72 <hany.gamal290@gmail.com>
2021-05-30 15:17:31 +0200

    pages/about: get and display about-us

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { GeneralService } from 'src/app/shared/services/general.service';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  settings = {} as any;
+  constructor(
+    private generalService: GeneralService,
+    public transalte: TranslateService
+  ) {
+    this.generalService.changeEmitted$.subscribe((data) => {
+      this.settings = data;
+    });
   }
 
+  ngOnInit(): void {}
 }
