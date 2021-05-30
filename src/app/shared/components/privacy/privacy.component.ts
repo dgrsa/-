40755206a3cecb960f734d1a8b6885243165eded
hanygamal72 <hany.gamal290@gmail.com>
2021-05-30 15:15:05 +0200

    shared/components/privacy: get and display privacy

--- src/app/shared/components/privacy/privacy.component.ts
+++ src/app/shared/components/privacy/privacy.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { GeneralService } from '../../services/general.service';
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
-  styleUrls: ['./privacy.component.scss']
+  styleUrls: ['./privacy.component.scss'],
 })
 export class PrivacyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  settings = {} as any;
+  constructor(
+    private generalService: GeneralService,
+    public transalte: TranslateService
+  ) {
+    this.generalService.changeEmitted$.subscribe((data) => {
+      this.settings = data;
+    });
   }
 
+  ngOnInit(): void {}
 }
