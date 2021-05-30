d546270d938997d88c3dfcac6cc236438cb80b00
hanygamal72 <hany.gamal290@gmail.com>
2021-05-30 15:15:23 +0200

    shared/components/terms: get and display terms

--- src/app/shared/components/terms-conditions/terms-conditions.component.ts
+++ src/app/shared/components/terms-conditions/terms-conditions.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { GeneralService } from '../../services/general.service';
 
 @Component({
   selector: 'app-terms-conditions',
   templateUrl: './terms-conditions.component.html',
-  styleUrls: ['./terms-conditions.component.scss']
+  styleUrls: ['./terms-conditions.component.scss'],
 })
 export class TermsConditionsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  settings = {} as any;
+  constructor(
+    private generalService: GeneralService,
+    public transalte: TranslateService
+  ) {
+    this.generalService.changeEmitted$.subscribe((data) => {
+      this.settings = data;
+    });
   }
 
+  ngOnInit(): void {}
 }
