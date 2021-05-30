a7343fe79c5af8326340cd76f0c85b1e023ead36
hanygamal72 <hany.gamal290@gmail.com>
2021-05-30 14:27:26 +0200

    shared/services/general: add getSettings method

--- src/app/shared/services/general.service.ts
+++ src/app/shared/services/general.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GeneralService {
+  private emitChangeSource = new Subject<any>();
+  changeEmitted$ = this.emitChangeSource.asObservable();
   constructor(private http: HttpClient) {}
+
+  emitChange(change: any) {
+    this.emitChangeSource.next(change);
+  }
 
   getBanners(resturant_id = undefined) {
     const params = {} as any;
@@ -29,5 +36,10 @@ export class GeneralService {
   callWaiter(sentData, resturantId, tableId) {
     const URL = `${environment.BASE_URL}/resturant/${resturantId}/table/${tableId}/call`;
     return this.http.post(URL, sentData);
+  }
+
+  getSettings() {
+    const URL = `${environment.BASE_URL}/setting`;
+    return this.http.get(URL);
   }
 }
