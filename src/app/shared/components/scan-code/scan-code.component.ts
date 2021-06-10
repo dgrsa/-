d3b5173841f39ab7d23b64bd450cb22a5367a537
hanygamal72 <hany.gamal290@gmail.com>
2021-06-10 11:40:51 +0200

    update scan function to new package

--- src/app/shared/components/scan-code/scan-code.component.ts
+++ src/app/shared/components/scan-code/scan-code.component.ts
@@ -5,8 +5,10 @@ import {
   Router,
   RoutesRecognized,
 } from '@angular/router';
+import { BarcodeFormat } from '@zxing/library';
 import { QrScannerComponent } from 'angular2-qrscanner';
 import { CookieService } from 'ngx-cookie-service';
+import { BehaviorSubject } from 'rxjs';
 import { filter, pairwise } from 'rxjs/operators';
 import { CartService } from '../../services/cart.service';
 
@@ -19,6 +21,26 @@ export class ScanCodeComponent implements OnInit {
   @ViewChild(QrScannerComponent, { static: true })
   qrScannerComponent: QrScannerComponent;
   previousUrl: string;
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+
+  qrResultString: string;
+
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
+
+  availableDevices: MediaDeviceInfo[];
+  deviceCurrent: MediaDeviceInfo;
+  deviceSelected: string;
+
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
   constructor(
     private router: Router,
     private cartService: CartService,
@@ -33,50 +55,93 @@ export class ScanCodeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.scanQrCode();
+    // this.scanQrCode();
   }
 
-  scanQrCode() {
-    this.qrScannerComponent.getMediaDevices().then((devices) => {
-      const videoDevices: MediaDeviceInfo[] = [];
-      for (const device of devices) {
-        if (device.kind.toString() === 'videoinput') {
-          videoDevices.push(device);
-        }
-      }
-      if (videoDevices.length > 0) {
-        let choosenDev;
-        for (const dev of videoDevices) {
-          if (dev.label.includes('back')) {
-            choosenDev = dev;
-            break;
-          }
-        }
-        if (choosenDev) {
-          this.qrScannerComponent.chooseCamera.next(choosenDev);
-        } else {
-          this.qrScannerComponent.chooseCamera.next(videoDevices[1]);
-        }
-      }
-    });
+  // scanQrCode() {
+  //   this.qrScannerComponent.getMediaDevices().then((devices) => {
+  //     const videoDevices: MediaDeviceInfo[] = [];
+  //     for (const device of devices) {
+  //       if (device.kind.toString() === 'videoinput') {
+  //         videoDevices.push(device);
+  //       }
+  //     }
+  //     if (videoDevices.length > 0) {
+  //       let choosenDev;
+  //       for (const dev of videoDevices) {
+  //         if (dev.label.includes('back')) {
+  //           choosenDev = dev;
+  //           break;
+  //         }
+  //       }
+  //       if (choosenDev) {
+  //         this.qrScannerComponent.chooseCamera.next(choosenDev);
+  //       } else {
+  //         this.qrScannerComponent.chooseCamera.next(videoDevices[1]);
+  //       }
+  //     }
+  //   });
 
-    this.qrScannerComponent.capturedQr.subscribe((result) => {
-      const splitingArr = result.split('tableId=');
-      this.cartService.tableNumber = result.split('?table=')[1].split('&')[0];
-      this.cartService.tableId = splitingArr[1];
-      this.cartService.resturantId = result.split('details/')[1].split('?')[0];
-      this.cookieService.set('tableId', this.cartService.tableId, 1);
-      this.cookieService.set('tableNumber', this.cartService.tableNumber, 1);
-      this.cookieService.set('resturantId', this.cartService.resturantId, 1);
-      if (this.previousUrl) {
-        this.router.navigate([this.previousUrl]);
+  //   this.qrScannerComponent.capturedQr.subscribe((result) => {
+  //     const splitingArr = result.split('tableId=');
+  //     this.cartService.tableNumber = result.split('?table=')[1].split('&')[0];
+  //     this.cartService.tableId = splitingArr[1];
+  //     this.cartService.resturantId = result.split('details/')[1].split('?')[0];
+  //     this.cookieService.set('tableId', this.cartService.tableId, 1);
+  //     this.cookieService.set('tableNumber', this.cartService.tableNumber, 1);
+  //     this.cookieService.set('resturantId', this.cartService.resturantId, 1);
+  //     if (this.previousUrl) {
+  //       this.router.navigate([this.previousUrl]);
+  //     } else {
+  //       if (result.includes('https://')) {
+  //         window.location.href = result;
+  //       } else {
+  //         window.location.href = `https://${result}`;
+  //       }
+  //     }
+  //   });
+  // }
+
+  onDeviceChange(device: MediaDeviceInfo) {
+    const selectedStr = device?.deviceId || '';
+    if (this.deviceSelected === selectedStr) {
+      return;
+    }
+    this.deviceSelected = selectedStr;
+    this.deviceCurrent = device || undefined;
+  }
+
+  onCodeResult(resultString: string) {
+    // this.qrResultString = resultString;
+    const splitingArr = resultString.split('tableId=');
+    this.cartService.tableNumber = resultString
+      .split('?table=')[1]
+      .split('&')[0];
+    this.cartService.tableId = splitingArr[1];
+    this.cartService.resturantId = resultString
+      .split('details/')[1]
+      .split('?')[0];
+    this.cookieService.set('tableId', this.cartService.tableId, 1);
+    this.cookieService.set('tableNumber', this.cartService.tableNumber, 1);
+    this.cookieService.set('resturantId', this.cartService.resturantId, 1);
+    if (this.previousUrl) {
+      this.router.navigate([this.previousUrl]);
+    } else {
+      if (resultString.includes('https://')) {
+        window.location.href = resultString;
       } else {
-        if (result.includes('https://')) {
-          window.location.href = result;
-        } else {
-          window.location.href = `https://${result}`;
-        }
+        window.location.href = `https://${resultString}`;
       }
-    });
+    }
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+    console.log(has);
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
   }
 }
